9a4fb524f25f191d4b4ce10803f0837d0acad95b
Kristofer Cagle <142164483+KrisCagle@users.noreply.github.com>
2026-02-25 15:47:37 -0600

    Refactor CityList to include city data and update HTML

--- scripts/CityList.js
+++ scripts/CityList.js
@@ -1,17 +1,28 @@
-import { getWalkers } from "./database.js"
+import { getWalkers, getCities } from "./database.js"
 
 const walkers = getWalkers()
+const cities = getCities()
+document.addEventListener(
+    "click",
+    (clickEvt) => {
+        const cityTarget = clickEvt.target
+        if (cityTarget.dataset.type === "city"){
 
+        window.alert(`${cityTarget.dataset.walkernames} is servicing this city`)
+        }
+    }
+)
 
 export const CityList = () => {
-    let citiesHTML = "<ol>"
+    let citiesHTML = "<ul>"
 
-     for (const walker of walkers) {
-         citiesHTML += `<li>${walker.city}</li>`
+    for (const city of cities) {
+        const cityWalkers = walkers.filter(walker => walker.cityId === city.id)
+        const walkerNames = cityWalkers.map(walker => walker.name).join(", ")
+
+        citiesHTML += `<li data-type="city" data-walkernames="${walkerNames}">${city.name}</li>`
     }
 
-    citiesHTML += "</ol>"
+    citiesHTML += "</ul>" 
 
     return citiesHTML
-}
-
